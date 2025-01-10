0944826b71297920b220cb4eb768081ef6bf5057
Vladimir Aleksic <vlada.pzs@hotmail.com>
2025-01-10 15:34:43 +0100

    Small fix regarding the directory comment

--- plainly-plugin/src/node/constants.ts
+++ plainly-plugin/src/node/constants.ts
@@ -4,30 +4,31 @@ const path = require('path');
 // @ts-ignore
 import CSInterface from './lib/CSInterface';
 
-export const homeDirectory = os.homedir();
+// csInterface related
 export const csInterface = new CSInterface();
 
-export const macDest = path.join(
+// directory related
+const homeDirectory = os.homedir();
+const macDest = path.join(
   homeDirectory,
   'Library',
   'Application Support',
   'Adobe',
   'Plainly Videos',
 );
-
-export const windowsDest = path.join(
+const windowsDest = path.join(
   homeDirectory,
   'AppData',
   'Roaming',
   'Adobe',
   'Plainly Videos',
 );
-
 const device: string = os.platform();
-
-export function getSettingsDirectory(): string {
+function getSettingsDirectory(): string {
   if (device === 'win32') {
     return windowsDest;
   }
   return macDest;
 }
+
+export const settingsDirectory = getSettingsDirectory();
