dcee28a0f9887f0b83b5717c22fcb792a0199a65
Vladimir Aleksic <vlada.pzs@hotmail.com>
2025-01-22 11:12:52 +0100

    remove log and change variable name

--- plainly-plugin/src/ui/components/form/UploadForm.tsx
+++ plainly-plugin/src/ui/components/form/UploadForm.tsx
@@ -30,15 +30,14 @@ export default function UploadForm({
   projectId,
   existing,
   badRevision,
-  error,
+  badDecrypt,
 }: {
   apiKey: string | undefined;
   projectId: string | undefined;
   existing?: boolean;
   badRevision?: boolean;
-  error?: boolean;
+  badDecrypt?: boolean;
 }) {
-  console.log(apiKey);
   const { notification, notifySuccess, notifyError, clearNotification } =
     useNotification();
 
@@ -52,7 +51,7 @@ export default function UploadForm({
   const [uploadMode, setUploadMode] = useState(existing ? 'edit' : 'new');
 
   const disableExisting = (!projectId || !existing) && uploadMode === 'edit';
-  const disabled = loading || !apiKey || disableExisting || error;
+  const disabled = loading || !apiKey || disableExisting || badDecrypt;
 
   const showBadRevision = badRevision && uploadMode === 'edit';
 
@@ -133,6 +132,13 @@ export default function UploadForm({
         {!apiKey && (
           <Alert
             title="To upload a project, you must have a valid API key set up in the settings."
+            type="danger"
+          />
+        )}
+
+        {badDecrypt && (
+          <Alert
+            title="To use this action, you need to provide a PIN for your API key."
             type="danger"
           />
         )}
