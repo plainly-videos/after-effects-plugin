9923255c22ad11517ea519d1d3b4ec32361d1053
Vladimir Aleksic <vlada.pzs@hotmail.com>
2025-02-18 14:17:31 +0100

    implement useNavigate after rebase

--- plainly-plugin/src/ui/components/common/Link.tsx
+++ plainly-plugin/src/ui/components/common/Link.tsx
@@ -1,12 +1,8 @@
+import { useNavigate } from '@src/ui/hooks';
+import type { Routes } from '@src/ui/types';
 import { handleLinkClick } from '@src/ui/utils';
 
-export function Link({
-  to,
-  text,
-  onClick,
-}: { to: string; text: string; onClick?: () => void }) {
-  // TODO: rebase and use useNavigate for internal links
-
+export function Link({ text, onClick }: { text: string; onClick: () => void }) {
   return (
     <button
       type="button"
@@ -19,5 +15,10 @@ export function Link({
 }
 
 export function ExternalLink({ to, text }: { to: string; text: string }) {
-  return <Link to={to} text={text} onClick={handleLinkClick.bind(null, to)} />;
+  return <Link text={text} onClick={handleLinkClick.bind(null, to)} />;
+}
+
+export function InternalLink({ to, text }: { to: Routes; text: string }) {
+  const { navigate } = useNavigate();
+  return <Link onClick={() => navigate(to)} text={text} />;
 }
