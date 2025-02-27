34a70d29b234f06644d0abd3ae588d7553b6c0fc
Vladimir Aleksic <vlada.pzs@hotmail.com>
2025-02-27 16:02:54 +0100

    Make it so bash script is callable from anywhere

--- upgrade-version.sh
+++ upgrade-version.sh
@@ -12,31 +12,34 @@ if ! [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   exit 1
 fi
 
+# Get the directory of the script, so it works from anywhere
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # Assign the version argument to a variable
 new_version="$1"
 
 updateVersion() {
     if [ "$(uname)" = "Darwin" ]; then
-        # macOs
+        # macOS
         sed -i "" "$1" "$2"
     else
-        # linux
+        # Linux
         sed -i "$1" "$2"
     fi
 }
 
 # Update the version field in the package.json files
-updateVersion "s/\"version\": \".*\"/\"version\": \"$new_version\"/" ./package.json
-updateVersion "s/\"version\": \".*\"/\"version\": \"$new_version\"/" plainly-aescripts/package.json
-updateVersion "s/\"version\": \".*\"/\"version\": \"$new_version\"/" plainly-plugin/package.json
+updateVersion "s/\"version\": \".*\"/\"version\": \"$new_version\"/" "$SCRIPT_DIR/package.json"
+updateVersion "s/\"version\": \".*\"/\"version\": \"$new_version\"/" "$SCRIPT_DIR/plainly-aescripts/package.json"
+updateVersion "s/\"version\": \".*\"/\"version\": \"$new_version\"/" "$SCRIPT_DIR/plainly-plugin/package.json"
 
 # Update the version in .env files
-updateVersion "s/PLUGIN_BUNDLE_VERSION=.*$/PLUGIN_BUNDLE_VERSION=$new_version/" plainly-plugin/.env.development
-updateVersion "s/PLUGIN_BUNDLE_VERSION=.*$/PLUGIN_BUNDLE_VERSION=$new_version/" plainly-plugin/.env.production
-updateVersion "s/PLUGIN_BUNDLE_VERSION=.*$/PLUGIN_BUNDLE_VERSION=$new_version/" plainly-plugin/.env.test
+updateVersion "s/PLUGIN_BUNDLE_VERSION=.*$/PLUGIN_BUNDLE_VERSION=$new_version/" "$SCRIPT_DIR/plainly-plugin/.env.development"
+updateVersion "s/PLUGIN_BUNDLE_VERSION=.*$/PLUGIN_BUNDLE_VERSION=$new_version/" "$SCRIPT_DIR/plainly-plugin/.env.production"
+updateVersion "s/PLUGIN_BUNDLE_VERSION=.*$/PLUGIN_BUNDLE_VERSION=$new_version/" "$SCRIPT_DIR/plainly-plugin/.env.test"
 
 # Update the version in manifest.xml
-updateVersion "s/ExtensionBundleVersion=\".*\"/ExtensionBundleVersion=\"$new_version\"/" ./CSXS/manifest.xml
-updateVersion "s/Id=\"com.plainlyvideos.after-effects-plugin.Panel\" Version=\".*\"/Id=\"com.plainlyvideos.after-effects-plugin.Panel\" Version=\"$new_version\"/" ./CSXS/manifest.xml
+updateVersion "s/ExtensionBundleVersion=\".*\"/ExtensionBundleVersion=\"$new_version\"/" "$SCRIPT_DIR/CSXS/manifest.xml"
+updateVersion "s/Id=\"com.plainlyvideos.after-effects-plugin.Panel\" Version=\".*\"/Id=\"com.plainlyvideos.after-effects-plugin.Panel\" Version=\"$new_version\"/" "$SCRIPT_DIR/CSXS/manifest.xml"
 
 echo "Version updated to $new_version"
