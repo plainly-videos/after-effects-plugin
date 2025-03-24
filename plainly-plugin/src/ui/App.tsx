dbdcd1137d7964becf2ed10ca90397230a6f2a88
Vladimir Aleksic <vlada.pzs@hotmail.com>
2025-03-24 13:57:01 +0100

    Run formater

--- plainly-plugin/src/ui/App.tsx
+++ plainly-plugin/src/ui/App.tsx
@@ -31,19 +31,19 @@ export function App() {
   }, [currentPage]);
 
   return (
-    <div className='h-screen w-screen flex flex-col overflow-hidden'>
+    <div className="h-screen w-screen flex flex-col overflow-hidden">
       {isDev && (
         <Button
           secondary
           onClick={reloadExtension}
-          className='absolute top-3 right-3 cursor-pointer z-40'
+          className="absolute top-3 right-3 cursor-pointer z-40"
         >
           Reload extension
         </Button>
       )}
       <Sidebar />
       <NoticeBanner show={showBanner}>
-        <p className='text-white text-xs font-medium'>
+        <p className="text-white text-xs font-medium">
           A new version is available! 🚀 See{' '}
           <ExternalLink
             to={`https://github.com/plainly-videos/after-effects-plugin/releases/tag/${data?.tag_name}`}
@@ -51,16 +51,14 @@ export function App() {
           />{' '}
           and{' '}
           <ExternalLink
-            to='https://exchange.adobe.com/apps/cc/202811/plainly-videos'
-            text='upgrade'
+            to="https://exchange.adobe.com/apps/cc/202811/plainly-videos"
+            text="upgrade"
           />
           .
         </p>
       </NoticeBanner>
 
-      <div className='flex-1 overflow-y-auto'>
-        {route}
-      </div>
+      <div className="flex-1 overflow-y-auto">{route}</div>
     </div>
   );
 }
