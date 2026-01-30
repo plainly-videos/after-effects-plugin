a9b7b78f47aa833a66ce4bf86031e81d10a57b7c
Vladimir Aleksic <vlada.pzs@hotmail.com>
2026-01-30 09:55:40 +0100

    remove unsued config

--- plainly-plugin/jest.config.cjs
+++ plainly-plugin/jest.config.cjs
@@ -3,9 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.ts'],
-  moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
-  },
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
