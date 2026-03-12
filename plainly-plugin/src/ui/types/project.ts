7e015d8036bbea2e640e4f4817cd32918fc3346e
Vladimir Aleksic <vlada.pzs@hotmail.com>
2026-03-12 14:19:38 +0100

    Enable Crop script adding and editing

--- plainly-plugin/src/ui/types/project.ts
+++ plainly-plugin/src/ui/types/project.ts
@@ -1,4 +1,5 @@
 import type { ModelBaseWithDates } from '.';
+import type { Template } from './template';
 
 export interface Project extends ModelBaseWithDates {
   analysis: ProjectAnalysis;
@@ -27,40 +28,4 @@ interface ProjectRevision {
   createdDate: string;
   expiredDate?: string;
   expired: boolean;
-}
-
-export interface Template {
-  id: string;
-  name: string;
-  layers: Layer[];
-}
-
-export interface Layer {
-  internalId: string;
-  layerName: string;
-  parametrization?: Parametrization;
-  scripting?: Scripting;
-}
-
-export interface Parametrization {
-  defaultValue?: string;
-  expression: boolean;
-  mandatory: boolean;
-  value: string;
-}
-
-export interface Scripting {
-  scripts: Script[];
-}
-
-export interface Script {
-  scriptType: ScriptType;
-}
-
-export enum ScriptType {
-  CROP = 'CROP',
-  MEDIA_AUTO_SCALE = 'MEDIA_AUTO_SCALE',
-  TEXT_AUTO_SCALE = 'TEXT_AUTO_SCALE',
-  SHIFT_IN = 'SHIFT_IN',
-  SHIFT_OUT = 'SHIFT_OUT',
 }
