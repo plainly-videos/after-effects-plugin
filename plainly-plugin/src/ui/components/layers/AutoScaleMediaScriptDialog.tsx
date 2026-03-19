930bd9604951a484ccdc714e21ae347340e78d69
Vladimir Aleksic <vlada.pzs@hotmail.com>
2026-03-19 10:30:33 +0100

    Add auto scale media script with fill and fixedRatio

---
+++ plainly-plugin/src/ui/components/layers/AutoScaleMediaScriptDialog.tsx
@@ -0,0 +1,108 @@
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/react';
+import { State, useGlobalState } from '@src/ui/state/store';
+import type { MediaAutoScaleScript } from '@src/ui/types/template';
+import classNames from 'classnames';
+import { ImageIcon } from 'lucide-react';
+import { useState } from 'react';
+import { Button, Checkbox } from '../common';
+import { Description } from '../typography';
+
+export function AutoScaleMediaScriptDialog({
+  mediaAutoScaleScript,
+  open,
+  setOpen,
+  action,
+}: {
+  mediaAutoScaleScript: MediaAutoScaleScript;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  action: (script: MediaAutoScaleScript) => void;
+}) {
+  const [settings] = useGlobalState(State.SETTINGS);
+  const sidebarOpen = settings.sidebarOpen;
+
+  const { fill, fixedRatio } = mediaAutoScaleScript;
+
+  const [fillState, setFillState] = useState(fill);
+  const [fixedRatioState, setFixedRatioState] = useState(fixedRatio);
+
+  const handleSave = () => {
+    action({
+      scriptType: mediaAutoScaleScript.scriptType,
+      fill: fillState,
+      fixedRatio: fixedRatioState,
+    });
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={open} onClose={setOpen} className="relative">
+      <DialogBackdrop className="fixed inset-0 backdrop-blur-md" />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            className={classNames(
+              'overflow-hidden rounded-lg bg-[rgb(29,29,30)] px-4 pb-4 pt-5 text-left sm:my-8 sm:w-full sm:max-w-lg sm:p-6 border border-white/10',
+              sidebarOpen ? 'ml-[3.75rem] xs:ml-36' : 'ml-[3.75rem]',
+            )}
+          >
+            <div className="flex flex-col gap-4">
+              <div className="flex items-start gap-2">
+                <div className="size-8 bg-indigo-500 rounded-md flex items-center justify-center">
+                  <ImageIcon className="size-4 text-white" />
+                </div>
+                <div>
+                  <DialogTitle
+                    as="h3"
+                    className="text-sm font-semibold text-white"
+                  >
+                    Auto scale media
+                  </DialogTitle>
+                  <Description>
+                    Automatically scale media layer to the composition size.
+                  </Description>
+                </div>
+              </div>
+              <Checkbox
+                label="Fill"
+                description="Fill the composition with the media asset."
+                defaultChecked={fillState}
+                onChange={setFillState}
+              />
+              <Checkbox
+                label="Fixed ratio"
+                description="Keep the aspect ratio of the media asset."
+                defaultChecked={fixedRatioState}
+                onChange={setFixedRatioState}
+              />
+            </div>
+            <div className="mt-4 sm:mt-3 sm:flex sm:flex-row-reverse">
+              <Button
+                type="button"
+                onClick={handleSave}
+                disabled={false}
+                className="inline-flex w-full sm:w-auto justify-center sm:ml-2"
+              >
+                Save
+              </Button>
+              <Button
+                type="button"
+                onClick={() => setOpen(false)}
+                secondary
+                className="inline-flex mt-2 sm:mt-0 w-full sm:w-auto justify-center"
+              >
+                Cancel
+              </Button>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
